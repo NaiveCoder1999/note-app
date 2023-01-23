83fd4d6bc5f8a25bbe6cf3c74882e867464e423d
Dexiang Chen <dexiang.chen@ucdconnect.ie>
2023-01-23 18:38:09 +0000

    add controller unit test of create

--- src/test/java/com/springbootcourse/controller/CourseControllerTest.java
+++ src/test/java/com/springbootcourse/controller/CourseControllerTest.java
@@ -11,7 +11,10 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -41,19 +44,36 @@ class CourseControllerTest {
     Course mockCourse = new Course(10001L, "NodeJS", "coder", "TestJava10001");
     String mockCourseJson = "{\"id\":10001,\"courseName\":\"NodeJS\",\"instructorName\":\"coder\",\"description\":\"TestJava10001\"}";
     @Test
-    void createCourse() {
+    void createCourse() throws Exception {
+        //Course course = new Course(10001L, "NodeJS", "coder", "TestJava10001");
+        Mockito.when(courseService.createCourse
+                (Mockito.any(Course.class))).thenReturn(mockCourse);
+
+        //invoke controller to create
+        RequestBuilder request = MockMvcRequestBuilders.
+                post("/instructors/coder/courses")
+                .content(mockCourseJson).contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus()); //compare 201 status code
+        assertEquals("http://localhost/instructors/coder/courses/10001",
+                response.getHeader(HttpHeaders.LOCATION));
+
     }
 
     @Test
     void getAllCourses() {
+
+
     }
 
     @Test
     void getCourse() throws Exception {
         Mockito.when(courseService.getCourse
-                (10001L, "coder")).thenReturn
-                (mockCourse);
+                (10001L, "coder")).thenReturn(mockCourse);
 
+        //invoke controller
         RequestBuilder request = MockMvcRequestBuilders.
                 get("/instructors/coder/courses/10001").accept(MediaType.APPLICATION_JSON);
 
