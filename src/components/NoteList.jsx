5e5afa97549e5152a39b5af5aebf41096ec8d318
Dexiang Chen <dexiang.chen@ucdconnect.ie>
2023-01-31 20:03:48 +0000

    initilize static list

---
+++ src/components/NoteList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function NoteList() {
+  return (
+    <div className="container">
+      <h3>All Notes</h3>
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Title</th>
+              <th scope="col">Notes</th>
+              <th scope="col">Update</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td scope="row">1</td>
+              <td>Java</td>
+              <td>Spring Boot and BootStrap</td>
+              <td>Delete</td>
+              <td>Update</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
--- src/components/UserApp.jsx
+++ src/components/UserApp.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import NoteList from './NoteList';
 
 export default function UserApp() {
   return (
     <>
       <h1> Note User Application</h1>
-      <h2> Note User</h2>
+      <NoteList />
     </>
   );
 }
