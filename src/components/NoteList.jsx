01372e213e7fb80da17caea8f7b3e91048b95a67
Dexiang Chen <dexiang.chen@ucdconnect.ie>
2023-02-08 00:48:08 +0000

    add buttons the list page

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -3,34 +3,20 @@ import * as Constants from '../constants/config';
 import { getAllNotes } from '../services/getAllNotes'; //non-default export
 
 export default function NoteList() {
-  const [courses, setCourses] = useState([]);
+  const [notes, setNotes] = useState([]);
   const [message, setMessage] = useState(null);
 
-  //async method
-  // async function refreshNotes() {
-  //   try {
-  //     const response = await getAllCourses(Constants.USER);
-  //     console.log(response);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
   function refreshNotes() {
     return getAllNotes(Constants.USER);
   }
 
-  // useEffect(() => {
-  //   const responseData = refreshNotes(); //invalid, no value
-  //   console.log(responseData.data);
-  //   setCourses(responseData.data);
-  // }, []);
-
   useEffect(() => {
     //use Immediately Invoked Function Expression(IIFE)
     (async () => {
-      let res = await refreshNotes();
-      console.log(res);
-      setCourses(res.data); //change promise to list
+      // var res = await refreshNotes();
+      let tableEntity = await refreshNotes(); //axiosresponse
+      console.log(tableEntity);
+      setNotes(tableEntity.data); //change promise to list
     })();
   }, []);
 
@@ -49,37 +35,29 @@ export default function NoteList() {
             </tr>
           </thead>
           <tbody>
-            {courses.map((course) => (
-              <tr key={course.id}>
-                <td>{course.id}</td>
-                <td>{course.courseName}</td>
-                <td>{course.description}</td>
-                <td>Delete</td>
-                <td>Update</td>
+            {notes.map((note) => (
+              <tr key={note.id}>
+                <td>{note.id}</td>
+                <td>{note.courseName}</td>
+                <td>{note.description}</td>
+                <td>
+                  <button
+                    data-testid="updateButton"
+                    className="btn btn-success"
+                  >
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button
+                    data-testid="deleteButton"
+                    className="btn btn-warning"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="container">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Title</th>
-              <th scope="col">Notes</th>
-              <th scope="col">Update</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td scope="row">1</td>
-              <td>Java</td>
-              <td>Spring Boot and BootStrap</td>
-              <td>Delete</td>
-              <td>Update</td>
-            </tr>
           </tbody>
         </table>
       </div>
