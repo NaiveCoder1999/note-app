6774021e590a22d15049b9911514e972454b1f10
Dexiang Chen <dexiang.chen@ucdconnect.ie>
2023-03-01 15:59:27 +0000

    modify useEffect fix note get. editor bug exists

--- src/components/NoteInfo.jsx
+++ src/components/NoteInfo.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import * as Constants from '../constants/config';
 import { updateNote } from '../services/updateNote';
 import { createNote } from '../services/createNote';
@@ -9,30 +9,35 @@ import { getSingleNote } from '../services/getSingleNote';
 import { generateHTML } from '@tiptap/html';
 import NoteForm from './NoteForm.jsx';
 
-import parser from 'html-react-parser';
-import * as Yup from 'yup';
-import Tiptap from './Tiptap.jsx';
 import '../styles/TiptapStyles.scss';
 
 export default function NoteInfo() {
+  const [noteData, setNoteData] = useState({
+    id: '',
+    noteName: '',
+    description: '',
+  }); //json
   const [title, setTitle] = useState(''); //string
   const [description, setDescription] = useState(''); //string of initial note
   const [preview, setPreview] = useState(''); //string of html data
-  const [placehoder, setPlacehoder] = useState('');
   //const [text, setText] = useState(''); //string of json data
   const { noteId } = useParams(); //noteId: "id"
 
+  const handleNoteInfo = useCallback(async () => {
+    getNoteInfo(Constants.USER, noteId);
+  }, [noteId]);
+  
   async function getNoteInfo(userName, id) {
     //get note json object
     let noteEntity = await getSingleNote(userName, id); //axios response type
-    //console.log(noteEntity);
+    console.log(noteEntity);
     let noteData = noteEntity.data;
+    setNoteData(noteData); //set a json data
     setTitle(noteData.noteName);
     setDescription(noteData.description);
   }
 
   //function to handle update and handle create
-  //TODO
   let navigate = useNavigate();
   function handleSubmit() {
     let username = Constants.USER;
@@ -64,18 +69,16 @@ export default function NoteInfo() {
           console.log(error);
         });
     }
-
     //console.log(values);
   }
-  //TODO refer kuroko
 
   useEffect(() => {
     if (noteId === `-1`) {
       return;
     }
 
-    getNoteInfo(Constants.USER, noteId);
-  });
+    handleNoteInfo();
+  }, [handleNoteInfo, noteId]);
 
   //TODO formik
   return (
@@ -84,24 +87,31 @@ export default function NoteInfo() {
       {/* <div>{noteId}</div>
       <div>{title}</div>
       <div>{description}</div> */}
+      <div>{noteData.id}</div>
+      <div>{noteData.noteName}</div>
+      <div>{noteData.description}</div>
       <p></p>
       <div className="container">
         <NoteForm
           id={noteId}
           title={title}
           description={description}
+          // id={noteData.id}
+          // title={noteData.noteName}
+          // description={noteData.description}
           onSubmit={handleSubmit}
           onNoteChange={setPreview} //update the note description realtime
         />
       </div>
 
+      {/* <p>For Testing:</p>
       <div className="Tiptap">
         <Tiptap
           initialValues={description}
           onChange={setPreview} //onchange function to pass HTML description to Note info component
           getHTML={setPlacehoder}
         />
-      </div>
+      </div> */}
       {/*
       <div>HTML render: {preview} </div>
       <div className="ProseMirror"> {parser(preview)} </div>
