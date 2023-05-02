c2d868bc4741d94654249d15f69278733b09d5ec
Dexiang Chen <dxchen1999@gmail.com>
2023-05-02 14:58:43 +0100

    add spring project for OAuth2 authorization

---
+++ springboot-note-authorization/src/main/java/com/authorization/SpringbootNoteAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNoteAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNoteAuthorizationApplication.class, args);
+	}
+
+}
